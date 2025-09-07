3d687b92e67da5998c7b97ea930198335344ea76
imane haffout <haffout.iman@gmail.com>
2025-09-07 19:18:51 +0100

    Update Hero.jsx

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,5 +1,5 @@
 import HeroText from "../components/HeroText";
-import ParallaxBackground from "../components/ParallaxBackground";
+import ParallaxBackground from "../components/ParallaxBackground.jsx";
 import { useMediaQuery } from "react-responsive";
 
 
